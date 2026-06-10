1f2c08d8951e91ebaf2913b0370722b6dc0b2b98
JunkDLC <junkdlc@gigeconomy.org.uk>
2026-06-10 10:51:27 +0100

    Try multipart email format.

--- ideas/formats/dialect.py
+++ ideas/formats/dialect.py
@@ -1,4 +1,6 @@
 import argparse
+import email
+import pathlib
 import pprint
 import tomllib
 import sys
@@ -17,12 +19,20 @@ def main(args):
             data = tomllib.loads(text)
             pprint.pprint(data, indent=1)
         return 0
+
+    for path in args.input:
+        if path.suffix == ".eml":
+            text = path.read_text()
+            msg = email.message_from_string(text)
+            for n, part in enumerate(msg.walk()):
+                if n:
+                    pprint.pprint(dict(part, n=n, text=str(part)))
     return 1
 
 
 def parser():
     rv = argparse.ArgumentParser(usage=__doc__, fromfile_prefix_chars="=")
-    rv.add_argument("input", nargs="*")
+    rv.add_argument("input", type=pathlib.Path, nargs="*")
     rv.convert_arg_line_to_args = lambda x: x.split()
     return rv
 
